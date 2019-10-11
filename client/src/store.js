d8d287e68ed0c8e77a874ba67b4995628008b414
AceIsHuman <menoman1995@yahoo.com>
2019-10-10 23:45:43 -0500

    add thunk and logger to store as middleware

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,6 @@
-import { createStore } from "redux";
-import { moviesReducer } from './reducers/MoviesReducer';
+import { createStore, applyMiddleware } from "redux";
+import { moviesReducer } from "./reducers/MoviesReducer";
+import thunk from 'redux-thunk';
+import logger from "redux-logger";
 
-export const store = createStore(moviesReducer);
+export const store = createStore(moviesReducer, applyMiddleware(thunk, logger));
