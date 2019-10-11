eec6061ec06ada25d29a087c7e34eb375371faa8
AceIsHuman <menoman1995@yahoo.com>
2019-10-10 21:42:02 -0500

    use axios to get movie details by id

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { Container, Form, Header } from "semantic-ui-react";
 
 const UpdateMovie = props => {
+  const { match, history} = props;
   const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/movies/${match.params.id}`)
+      .then(res => {
+        setMovie(res.data);
+      })
+      .catch(err => console.error(err.response));
+  }, [match.params.id]);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -10,12 +21,12 @@ const UpdateMovie = props => {
 
   return (
     <Container>
-      <Form medium>
-        <Form.Input name="title" placeholder="Title" />
-        <Form.Input name="director" placeholder="Director" />
-        <Form.Input name="metascore" placeholder="Metascore"/>
+      <Form>
+        <Form.Input name="title" placeholder="Title" value={movie && movie.title} />
+        <Form.Input name="director" placeholder="Director" value={movie && movie.director} />
+        <Form.Input name="metascore" placeholder="Metascore" value={movie && movie.metascore} />
         {movie ? (
-          <div class="stars">
+          <div className="stars">
             <Header as="h4">Actors</Header>
             {movie.stars.map((star, i) => (
               <Form.Input key={("star ", i)} name="star" value={star} />
